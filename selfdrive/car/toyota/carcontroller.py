6d597c07a3367df4b1b30cbdd3b986aabb112f6e
Nick Brown <njbgaming09@gmail.com>
2020-10-12 03:15:29 -0400

    Update carcontroller.py

--- selfdrive/car/toyota/carcontroller.py
+++ selfdrive/car/toyota/carcontroller.py
@@ -174,7 +174,7 @@ class CarController():
     
     #Test set steer to 100
     actuators.steer = -joystick_steer
-    actuators.steerAngle = -joystick_steer * 360.
+    actuators.steerAngle = -joystick_steer * 43.
     actuators.gas = joystick_accel
     actuators.brake = joystick_brake
     
